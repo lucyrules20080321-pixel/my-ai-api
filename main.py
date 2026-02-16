a1a6c97317081106803fe7897e49ef0945222b24
lucyrules20080321-pixel <lucyrules20080321@gmail.com>
2026-02-17 01:43:48 +0800

    main.py

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ app = FastAPI()
 # 自动从系统读取你的秘密通行证
 GEMINI_KEY = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_KEY)
-model = 
-genai.GenerativeModel('models/gemini-1.5-flash')
+model = genai.GenerativeModel('models/gemini-1.5-flash')
 @app.get("/")
 def home():
     return {"message": "我的手机手搓API已上线！"}
-
 @app.get("/chat")
 def chat(q: str):
     response = model.generate_content(q)
